85f081edcbc1c311eb507f3d23fc7504a9d1dddc
yeabtsega45 <yabtsega2022@gmail.com>
2023-09-19 14:47:37 +0300

    fetching api

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,8 +1,54 @@
+import { gql } from "@/node_modules/graphql-request/build/esm/index"
+
 const fetchNews = async (
     category?: Category | string,
     keywords?: string,
     isDynamic?: boolean,
 ) => {
+    //GraphQL query
+    const query = gql`
+    query MyQuery(
+        $access_key: String!
+        $categories: String!
+        $keywords: String
+        ) {
+            myQuery(
+                access_key: $access_key
+                countries: "gb"
+                categories: $categories
+                sort: "published_desk"
+                keywords: $keywords
+              ) {
+                data {
+                    author
+                    category
+                    country
+                    description
+                    image
+                    language
+                    published_at
+                    source
+                    title
+                    url
+                }
+                pagination {
+                    count
+                    limit
+                    offset
+                    total
+                }
+              }
+        }`;
+    //Fetch function with Next.js 13 caching
+    const res = await fetch("https://oliveiradeazemeis.stepzen.net/api/endpoint/__graphql",{
+        method:"POST",
+        cache: isDynamic ? "no-cache" : "default",
+        next: isDynamic ? {revalidate:0} : {revalidate:20},
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+        }
+    })    
 }
 
 export default fetchNews
